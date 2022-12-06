b8b53b95a896d06321c49eb014d88f02de108d87
Peter G. Morales <pgmorales76@gmail.com>
2022-12-05 19:19:31 -0500

    Modified file path for images

--- scripts/app_01.js
+++ scripts/app_01.js
@@ -22,25 +22,25 @@ function Product(name, src) {
 }
 
 // instances of constructor object
-let product_01 = new Product('bag', './images/bag.jpg');
-let product_02 = new Product('banana', './images/banana.jpg');
-let product_03 = new Product('bathroom', './images/bathroom.jpg');
-let product_04 = new Product('boots', './images/boots.jpg');
-let product_05 = new Product('breakfast', './images/breakfast.jpg');
-let product_06 = new Product('bubblegum', './images/bubblegum.jpg');
-let product_07 = new Product('chair', './images/chair.jpg');
-let product_08 = new Product('cthulhu', './images/cthulhu.jpg');
-let product_09 = new Product('dog_duck', './images/dog-duck.jpg');
-let product_10 = new Product('dragon', './images/dragon.jpg');
-let product_11 = new Product('pen', './images/pen.jpg');
-let product_12 = new Product('pet_sweep', './images/pet-sweep.jpg');
-let product_13 = new Product('scissors', './images/scissors.jpg');
-let product_14 = new Product('shark', './images/shark.jpg');
-let product_15 = new Product('sweep', './images/sweep.png');
-let product_16 = new Product('tauntaun', './images/tauntaun.jpg');
-let product_17 = new Product('unicorn', './images/unicorn.jpg');
-let product_18 = new Product('water_can', './images/water-can.jpg');
-let product_19 = new Product('wine_glass', './images/wine-glass.jpg');
+let product_01 = new Product('bag', 'images/bag.jpg');
+let product_02 = new Product('banana', 'images/banana.jpg');
+let product_03 = new Product('bathroom', 'images/bathroom.jpg');
+let product_04 = new Product('boots', 'images/boots.jpg');
+let product_05 = new Product('breakfast', 'images/breakfast.jpg');
+let product_06 = new Product('bubblegum', 'images/bubblegum.jpg');
+let product_07 = new Product('chair', 'images/chair.jpg');
+let product_08 = new Product('cthulhu', 'images/cthulhu.jpg');
+let product_09 = new Product('dog_duck', 'images/dog-duck.jpg');
+let product_10 = new Product('dragon', 'images/dragon.jpg');
+let product_11 = new Product('pen', 'images/pen.jpg');
+let product_12 = new Product('pet_sweep', 'images/pet-sweep.jpg');
+let product_13 = new Product('scissors', 'images/scissors.jpg');
+let product_14 = new Product('shark', 'images/shark.jpg');
+let product_15 = new Product('sweep', 'images/sweep.png');
+let product_16 = new Product('tauntaun', 'images/tauntaun.jpg');
+let product_17 = new Product('unicorn', 'images/unicorn.jpg');
+let product_18 = new Product('water_can', 'images/water-can.jpg');
+let product_19 = new Product('wine_glass', 'images/wine-glass.jpg');
 
 // products array
 let products = [product_01, product_02, product_03, product_04, product_05,
